ba02ad8a7884700f142c578b519229a71e68c184
Enyewashebr <Enyewashebir0@gmail.com>
2025-10-16 15:32:47 +0300

    Implemented MongoDB connection and updateed environment variables

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import { PORT } from './config/env.js';
 import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
+import connectDB  from './database/mongodb.js';
 
 const app = express();
 
@@ -15,8 +16,11 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+
+await connectDB();
+
 });
 
 
